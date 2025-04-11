912aa09251af1fdac0a0f3b7f73c404965cee862
Ankit <ankitsharma.as420@gmail.com>
2025-04-11 14:18:35 +0530

    added role field

--- java-springboot-securitybatch2/src/main/java/com/example/java/springboot/securitybatch2/Model/UserRegistration.java
+++ java-springboot-securitybatch2/src/main/java/com/example/java/springboot/securitybatch2/Model/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.example.java.springboot.securitybatch2.Model;
 
+import com.example.java.springboot.securitybatch2.Entity.Role;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,5 @@ public class UserRegistration {
     private String lastName;
     private String email;
     private String password;
+    private Role rol;
 }
